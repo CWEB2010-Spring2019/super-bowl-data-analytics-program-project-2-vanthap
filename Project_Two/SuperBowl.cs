ab35c6ff1b2cad3025c7a7042769536b0e7b672b
Vang, Thao <46498748+vanthap@users.noreply.github.com>
2019-02-28 18:21:02 -0600

    Add files via upload
    test

---
+++ Project_Two/SuperBowl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project2
+{
+    public class SuperBowl
+    {
+        public string date { get; set; }
+        public string sb { get; set; }
+        public int attendance { get; set; }
+        public string winnerQB { get; set; }
+        public string winnerCoach { get; set; }
+        public string winner { get; set; }
+        public int winnerPoints { get; set; }
+        public string loserQB { get; set; }
+        public string loserCoach { get; set; }
+        public string loser { get; set; }
+        public int loserPoints { get; set; }
+        public string MVP { get; set; }
+        public string stadium { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+
+    }
+
+}
