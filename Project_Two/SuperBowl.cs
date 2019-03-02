a0a506ac9dfd205fb5c45199615011689971d57f
vanthap <vanthap@dunwoody.edu>
2019-03-01 18:18:28 -0600

    new

---
+++ Project_Two/SuperBowl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Two
+{
+    class SuperBowl
+    {
+        public class SBD
+        {
+            public string date { get; set; }
+            public string sb { get; set; }
+            public int attendance { get; set; }
+            public string qbWinner { get; set; }
+            public string coachWinner { get; set; }
+            public string winner { get; set; }
+            public int winnerPts { get; set; }
+            public string qbLoser { get; set; }
+            public string coachLoser { get; set; }
+            public string loser { get; set; }
+            public int loserPts { get; set; }
+            public string mvp { get; set; }
+            public string stadium { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+        }
+    }
+}
---
+++ Project_Two/SuperBowlData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Project_Two
+{
+    class SuperBowlData
+    {
+
+		static void Main(string[] args)
+		{
+            /**Your application should allow the end user to pass a file path for output 
+			* or guide them through generating the file.
+			**/
+
+            string filePath = Directory.GetCurrentDirectory();
+            string backOne = Directory.GetParent(filePath).ToString();
+            string backTwo = Directory.GetParent(backOne).ToString();
+            string backThree = Directory.GetParent(backTwo).ToString();
+            string SBData = @"E:\P2\Project_Two\Super_Bowl_Project.csv";
+            Console.WriteLine(backThree);
+
+            if (Directory.Exists(backThree))
+            {
+                Console.WriteLine("It exists.");
+                if (File.Exists(SBData))
+                {
+                    Console.WriteLine("It exists.");
+                    List<SuperBowl> sbList = new List<SuperBowl>();
+
+                    FileStream csvFile = new FileStream(SBData, FileMode.Open, FileAccess.Read);
+                    StreamReader reader = new StreamReader(csvFile);
+
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Doesn't exists.");
+            }
+
+
+		}
+	}
+
+
+	
+}
